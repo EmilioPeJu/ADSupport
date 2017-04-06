33d86b528689a732191b4d571440b9f8c9f4ebd2
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-04-06 15:26:30 +0100

    Update builder file     * Remove Nexus and netCDF libraries     * Change libxml2 -> xml2 for change upstream

--- etc/builder.py
+++ etc/builder.py
@@ -18,15 +18,15 @@ class ADSupport(Device):
             # MSVC2010. So we cant use them.
             usemagic = False
         usehdfdlls = True
-        hdfdlls = ['zlib', 'szip', 'hdf5', 'libxml2']
-        hdfstatic = ['zlib', 'libszip', 'hdf5', 'libxml2']
+        hdfdlls = ['zlib', 'szip', 'hdf5', 'xml2']
+        hdfstatic = ['zlib', 'libszip', 'hdf5', 'xml2']
         magiclibs = ['CORE_RL_zlib_',
             'CORE_RL_xlib_', 'CORE_RL_wmf_', 'CORE_RL_ttf_', 'CORE_RL_tiff_',
             'CORE_RL_png_', 'CORE_RL_libxml_', 'CORE_RL_lcms_', 'CORE_RL_jpeg_',
             'CORE_RL_jp2_', 'CORE_RL_jbig_', 'CORE_RL_bzlib_',
             'CORE_RL_filters_', 'CORE_RL_coders_', 'CORE_RL_magick_',
             'CORE_RL_wand_', 'CORE_RL_Magick++_']
-        LibFileList = ['NeXus'] #, 'PvAPI']
+        LibFileList = []
         if usemagic:
             LibFileList += magiclibs
         else:
@@ -37,7 +37,7 @@ class ADSupport(Device):
             LibFileList = hdfstatic + LibFileList
         
         # TODO these should also be protected by a 'use' clause
-        LibFileList += ['netCDF', 'ADBase', 'NDPlugin']
+        LibFileList += ['ADBase', 'NDPlugin']
         
         SysLibFileList = ['Oleaut32', 'Gdi32']
 
